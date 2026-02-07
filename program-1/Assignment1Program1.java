e2aa98ba0956835e6ab2a6c3d0835f5dc2fa9877
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-02-07 12:06:55 -0500

    Finished block letters

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,12 @@
 public class Assignment1Program1 {
   public static void main(String[] args) {
-
+    System.out.println("  SSSSSSSS         SSSSSSSS");
+    System.out.println("SSS      SSS     SSS      SSS");
+    System.out.println("SSS              SSS");
+    System.out.println("  SSSSSS           SSSSSS");
+    System.out.println("       SSS              SSS");
+    System.out.println("         SSS              SSS");
+    System.out.println(" SSS     SSS     SSS     SSS");
+    System.out.println("   SSSSSSS         SSSSSSS");
   }
 }
