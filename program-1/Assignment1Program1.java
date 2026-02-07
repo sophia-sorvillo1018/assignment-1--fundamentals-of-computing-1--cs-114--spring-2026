787062bdf231869b478004b7acfb10f81597d74f
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-02-07 15:20:17 -0500

    Finished user imput 5-character string

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
     System.out.println("  SSSSSSSS         SSSSSSSS");
@@ -6,7 +8,12 @@ public class Assignment1Program1 {
     System.out.println("  SSSSSS           SSSSSS");
     System.out.println("       SSS              SSS");
     System.out.println("         SSS              SSS");
-    System.out.println(" SSS     SSS     SSS     SSS");
+    System.out.println(" SSS     SSS     gitSSS     SSS");
     System.out.println("   SSSSSSS         SSSSSSS");
+    System.out.println("");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a 5-character string: ");
+    String fiveCharacterString = input.nextLine();
+    System.out.println(" ");
   }
 }
