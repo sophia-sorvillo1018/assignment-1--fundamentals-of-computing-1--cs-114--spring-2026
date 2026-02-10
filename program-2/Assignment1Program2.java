b8aac27b35c8a63c1ae53e1ba9782c06f9ee59c2
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-02-10 16:58:27 -0500

    Added formula for figuring out maximum number

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -4,7 +4,8 @@ public class Assignment1Program2 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Please enter a base (2 - 9): ");
-    String baseNum = input.next();
-    System.out.println("The maximum, 4-digit, base 10 number in base " + baseNum + " is ");
+    int baseNum = input.nextInt();
+    int maxNumUsingBase = (baseNum - 1) * ((baseNum * baseNum * baseNum) + (baseNum * baseNum) + baseNum + 1);
+    System.out.println("The maximum, 4-digit, base 10 number in base " + baseNum + " is " + maxNumUsingBase);
   }
 }
