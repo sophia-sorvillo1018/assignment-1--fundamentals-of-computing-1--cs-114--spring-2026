e20a6643428158ff40e874aec23e88680f69b2a3
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-02-12 16:14:11 -0500

    Cleaned up strings

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -10,21 +10,21 @@ public class Assignment1Program1 {
     System.out.println("         SSS              SSS");
     System.out.println(" SSS     SSS      SSS     SSS");
     System.out.println("   SSSSSSS          SSSSSSS");
-    System.out.println("");
+    System.out.println();
     Scanner input = new Scanner(System.in);
     System.out.println("Please enter a 5-character string: ");
     String fiveCharacterString = input.nextLine();
     char thirdInSequence = fiveCharacterString.charAt(1);
     char secondInSequence = fiveCharacterString.charAt(2);
     char firstInSequence = fiveCharacterString.charAt(3);
-    System.out.println(" ");
+    System.out.println();
     System.out.println("Please enter a number in Fahrenheit: ");
     double tempFahrenheit = input.nextInt();
     double tempCelsius = ((tempFahrenheit - 32)* 5)/9;
-    System.out.println(" ");
+    System.out.println();
     input.close();
     System.out.println("Random number generated. Continuing...");
-    System.out.println(" ");
+    System.out.println();
     int randomNum = 32 + (int)(Math.random() * ((16384 - 32) + 1));
     System.out.println("Your new string is " + tempCelsius + firstInSequence + secondInSequence + thirdInSequence + randomNum);
   }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -7,5 +7,7 @@ public class Assignment1Program2 {
     int baseNum = input.nextInt();
     int maxNumUsingBase = (baseNum - 1) * ((baseNum * baseNum * baseNum) + (baseNum * baseNum) + baseNum + 1);
     System.out.println("The maximum, 4-digit, base 10 number in base " + baseNum + " is " + maxNumUsingBase);
+    System.out.println("Now, enter a base 10 number in the range 0 to " + maxNumUsingBase + " to convert: ");
+    int numToCovert = input.nextInt();
   }
 }
