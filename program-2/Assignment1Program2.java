ff9b0468c78c7c4f6c242530c2e1e9c7e7d06228
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-02-14 18:10:41 -0500

    finished converting base equation.

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -8,6 +8,16 @@ public class Assignment1Program2 {
     int maxNumUsingBase = (baseNum - 1) * ((baseNum * baseNum * baseNum) + (baseNum * baseNum) + baseNum + 1);
     System.out.println("The maximum, 4-digit, base 10 number in base " + baseNum + " is " + maxNumUsingBase);
     System.out.println("Now, enter a base 10 number in the range 0 to " + maxNumUsingBase + " to convert: ");
-    int numToCovert = input.nextInt();
+    int numToConvert = input.nextInt();
+    int covertingNumForEquation = numToConvert;
+    int newBase = 0;
+    int placeValue = 1;
+    while (covertingNumForEquation > 0) {
+      int remainder = covertingNumForEquation % baseNum;
+      newBase += remainder * placeValue;
+      placeValue *= 10;
+      covertingNumForEquation /= baseNum;
+    }
+    System.out.println(numToConvert + "(base 10) = " + newBase + " (base " + baseNum + ")");
   }
 }
