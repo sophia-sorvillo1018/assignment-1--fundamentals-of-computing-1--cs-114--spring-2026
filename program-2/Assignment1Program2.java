01d874725b1aa8a65686b201fdabda9acce105b9
sophia-sorvillo1018 <sophialily1018@gmail.com>
2026-02-18 18:27:06 -0500

    Neatened up code

--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class Assignment1Program2 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+
     System.out.println("Please enter a base (2 - 9): ");
     int baseNum = input.nextInt();
+    System.out.println();
+
     int maxNumUsingBase = (baseNum - 1) * ((baseNum * baseNum * baseNum) + (baseNum * baseNum) + baseNum + 1);
     System.out.println("The maximum, 4-digit, base 10 number in base " + baseNum + " is " + maxNumUsingBase);
+    System.out.println();
+
     System.out.println("Now, enter a base 10 number in the range 0 to " + maxNumUsingBase + " to convert: ");
     int numToConvert = input.nextInt();
+    System.out.println();
+
     int numToConvertForEquation = numToConvert;
     int newBase = 0;
     int placeValue = 1;
